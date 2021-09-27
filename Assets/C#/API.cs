1c90afaa9696f4b53ec04d047ddd4dd44c8ef7e2
Eric Chang <89112536+Eric504060304@users.noreply.github.com>
2021-09-27 09:45:33 +0800

    API_1

--- Assets/C#/API.cs
+++ Assets/C#/API.cs
@@ -7,12 +7,20 @@ public class API : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        #region 靜態屬性
+        //取得
+        //語法
+        //類別名稱,靜態屬性
+        float r = Random.value;
+        print("取得靜態屬性，隨機值:" + r);
+        #endregion
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        #region 靜態屬性
+        print("經過多久" + Time.timeSinceLevelLoad);
+        #endregion
     }
 }
