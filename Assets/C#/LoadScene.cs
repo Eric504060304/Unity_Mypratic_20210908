800ef0439a433762acbdc4f3063e68278f300269
Eric Chang <89112536+Eric504060304@users.noreply.github.com>
2021-12-06 09:53:18 +0800

    渲染OK

---
+++ Assets/C#/LoadScene.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadScene : MonoBehaviour
+{
+   public void StartGame()
+    {
+        Application.LoadLevel("¹CÀ¸³õ´º");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
